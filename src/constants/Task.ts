950381d5f394197adde854b76fdfeb2082996ba5
mohit choudhary <mohit.choudhary@appdynamics.com>
2020-10-14 00:52:09 +0530

    nit

--- src/constants/Task.ts
+++ src/constants/Task.ts
@@ -1,5 +1,5 @@
 export class Task {
-    public static readonly LIST_NAME: string = "TESTQNET";
+    public static readonly LIST_NAME: string = "QNET";
     public static readonly MAX_TASK_UPDATE: number = 10;
     public static readonly MAX_TASK_CREATE: number = 100;
 }
