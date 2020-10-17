5d71f73fc7223066a76da3fa2b48e44e55a6b486
mohit choudhary <mohit.choudhary@appdynamics.com>
2020-10-17 13:50:41 +0530

    bug

--- src/constants/Calender.ts
+++ src/constants/Calender.ts
@@ -1,4 +1,4 @@
 export class Calender {
     public static readonly MAX_EVENT_DELETE: number = 50;
-    public static readonly SKIP_CALENDER: [];
+    public static readonly SKIP_CALENDER: ["Holidays in India"];
 }
