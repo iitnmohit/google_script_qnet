74e96288f99aab6eef61910e8380a2582ac1c27b
mohit choudhary <mohit.choudhary@appdynamics.com>
2020-10-23 18:02:22 +0530

    doc in theme util, compile may fail

--- src/util/ThemeUtil.ts
+++ src/util/ThemeUtil.ts
@@ -1,6 +1,9 @@
 import { Themes } from "../constants/Themes";
 import { ITheme } from "../interface/ITheme";
-
+/**
+ * used for setting and getting currect theme.
+ * if not set it will return default theme.
+ */
 export class ThemeUtil {
     private static currentTheme: ITheme = Themes.DEFAULT;
 
