af42a532683c8ede7171b654f637e9b823e59e49
mohit choudhary <mohit.choudhary@appdynamics.com>
2020-11-04 14:06:24 +0530

    nit

--- src/constants/Log.ts
+++ src/constants/Log.ts
@@ -4,7 +4,9 @@ export class Log {
     public static readonly MAX_LOG_UPDATE: number = 50;
     // this will add new line above replacement in log
     public static readonly TEXT_TO_REPLACE_FUNCTION_MAP: Map<string, (todayDate: Date) => string> = new Map([
-        ["today", DateUtil.format]
+        ["today", (todaysDate: Date): string => {
+            return DateUtil.format(todaysDate);
+        }]
     ]);
     // this will simply replace line as below in log
     public static readonly TEXT_TO_REPLACE_MAP: Map<string, string> = new Map([
